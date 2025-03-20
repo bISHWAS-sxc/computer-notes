c774070f0f1968bad8e739b14642de722c41dd26
bISHWAS-sxc <024neb955@sxc.edu.np>
2025-03-20 11:24:17 +0545

    Add files via upload

--- floatjs.js
+++ floatjs.js
@@ -1,4 +1,4 @@
-<script>
+
 // Redirect to chat.html when the floating button is clicked
 document.getElementById("floating-button").addEventListener("click", function() {
     window.location.href = "chat.html"; // Redirect to chat.html
